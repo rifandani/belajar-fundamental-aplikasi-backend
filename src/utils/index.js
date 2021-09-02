6d714acd63eea507eb89351418b5b15021403716
rifandani <ipandani2505@gmail.com>
2021-09-02 22:04:29 +0700

    refactor(utils): delete mapPlaylistSongsDBToModel function

--- src/utils/index.js
+++ src/utils/index.js
@@ -20,13 +20,6 @@ const mapSongsDBToModel = ({
   updatedAt: updated_at,
 });
 
-const mapPlaylistSongsDBToModel = ({ id, playlist_id, song_id }) => ({
-  id,
-  playlistId: playlist_id,
-  songId: song_id,
-});
-
 module.exports = {
   mapSongsDBToModel,
-  mapPlaylistSongsDBToModel,
 };
